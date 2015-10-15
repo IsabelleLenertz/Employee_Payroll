c0468c411206ab30aedef809b2dbe0d42535e16d
Isabelle Delmas <isabelle.lenertz@gmail.com>
2015-10-14 20:42:55 -0700

    Created project.

---
+++ src/EmployeePayroll.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : EmployeePayroll.cpp
+// Author      : Isabelle Delmas
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
