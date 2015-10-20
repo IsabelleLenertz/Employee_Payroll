1e593547dd02c156bcd39d21c1add3a2d27963d2
Isabelle Delmas <isabelle.lenertz@gmail.com>
2015-10-19 23:13:43 -0700

    EmployeePayroll.cpp getUserChoice (menu + user input) implemented and tested

--- src/EmployeePayroll.cpp
+++ src/EmployeePayroll.cpp
@@ -7,9 +7,43 @@
 //============================================================================
 
 #include <iostream>
+#include "Utilities.hpp"
+#include "Employee.hpp"
+#include "ProductionWorker.hpp"
+#include "ShiftSupervisor.hpp"
+#include "TeamLeader.hpp"
+
 using namespace std;
 
+int getUserChoice();
+
 int main() {
-	cout << "" << endl; // prints 
+	int userChoice = 0; //use to stroe user's input.
+
+
+	userChoice = getUserChoice();
+	cout << userChoice;
 	return 0;
+}
+
+int getUserChoice()
+{
+	// Prints the header
+	string welcome = "Welcome to the Employee Pay System";
+	cout << welcome << endl;
+	for (int i=0; i< (int)welcome.size(); i++){
+		cout << "=";
+	}
+	cout << endl;
+
+	// Prints the menu
+	cout << "1. Enter Production Worker Information" << endl;
+	cout << "2. Enter Shift Supervisor Information" << endl;
+	cout << "3. Enter Team Leader Information" << endl;
+	cout << "4. Pay Everyone" << endl;
+	cout << "9. Exit Program" << endl;
+
+	//Asks the used for input and returns it
+	return Utilities::inputInt("Please enter a menu option :", 1, 4, 9);
+
 }
